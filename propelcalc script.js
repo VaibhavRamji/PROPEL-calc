58ff2a1ab462c186dca1ad0126cb2aace80b220a
VaibhavRamji <vaibhav.r.iyengar@gmail.com>
2025-01-23 09:40:35 +0530

    add: adds option to calculate oxidizer to fuel ratio

--- propelcalc script.js
+++ propelcalc script.js
@@ -9,4 +9,14 @@ document.addEventListener('DOMContentLoaded'), (event) => {
         const specificGasContent = parseFloat(document.getElementById('specificGasContent').value);
         const expansionRatio = parseFloat(document.getElementById('expansionRatio').value);
         const massFlowRate = parseFloat(document.getElementById('massFlowRate').value);
-        const nozzleExitPressure = parseFloat(document.getElementById('nozzleExitPressure').value);}
+        const nozzleExitPressure = parseFloat(document.getElementById('nozzleExitPressure').value)
+        
+         // Constants
+         const R = 8314; // J/(kmol*K)
+         const eta = 0.95; // Nozzle efficiency
+         const g0 = 9.81; // m/s^2 (standard gravitational acceleration)
+        
+        
+        ;}
+
+       
